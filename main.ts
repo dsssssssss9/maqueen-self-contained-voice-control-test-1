7d9058c7379285453e27dcaf0e4f48906aa4ccae
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-04-20 21:12:26 +0100

    add 2 commands to stop.move to top of if loop

--- main.ts
+++ main.ts
@@ -6,23 +6,26 @@ serial.writeLine("" + (voiceRecognition.getWakeTime()))
 voiceRecognition.playByCMDID(voiceRecognition.checkWord1(voiceRecognition.WakeupWords.W2))
 serial.writeLine("==================")
 maqueen.motorStop(maqueen.Motors.All)
-basic.showIcon(IconNames.Yes)
+basic.showIcon(IconNames.Ghost)
 basic.forever(function () {
     voiceRecognition.getCMDID()
     if (voiceRecognition.checkCMDID()) {
-        if (voiceRecognition.readCMDID() == voiceRecognition.checkWord3(voiceRecognition.FixedCommandWords.W62)) {
-            basic.showIcon(IconNames.Happy)
-        } else if (voiceRecognition.readCMDID() == voiceRecognition.checkWord3(voiceRecognition.FixedCommandWords.W65)) {
-            basic.clearScreen()
-        } else if (voiceRecognition.readCMDID() == voiceRecognition.checkWord2(voiceRecognition.LearningCommandWords.W5)) {
+        if (voiceRecognition.readCMDID() == voiceRecognition.checkWord3(voiceRecognition.FixedCommandWords.W24)) {
             maqueen.motorStop(maqueen.Motors.All)
             basic.showIcon(IconNames.No)
+        } else if (voiceRecognition.readCMDID() == voiceRecognition.checkWord2(voiceRecognition.LearningCommandWords.W5)) {
+            maqueen.motorStop(maqueen.Motors.All)
+            basic.showString("S")
+        } else if (voiceRecognition.readCMDID() == voiceRecognition.checkWord3(voiceRecognition.FixedCommandWords.W62)) {
+            basic.showIcon(IconNames.Happy)
         } else if (voiceRecognition.readCMDID() == voiceRecognition.checkWord3(voiceRecognition.FixedCommandWords.W22)) {
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
             basic.showString("F")
         } else if (voiceRecognition.readCMDID() == voiceRecognition.checkWord3(voiceRecognition.FixedCommandWords.W23)) {
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 30)
             basic.showString("R")
+        } else if (voiceRecognition.readCMDID() == voiceRecognition.checkWord3(voiceRecognition.FixedCommandWords.W65)) {
+            basic.clearScreen()
         }
     }
 })
