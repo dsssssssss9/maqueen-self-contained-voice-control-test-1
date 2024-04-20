23380f874e4d85111a6d25296116e68c9ae851bd
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-04-20 19:43:11 +0100

    add maqueen + basic move stop test

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ voiceRecognition.setWakeTime(45)
 serial.writeLine("" + (voiceRecognition.getWakeTime()))
 voiceRecognition.playByCMDID(voiceRecognition.checkWord1(voiceRecognition.WakeupWords.W2))
 serial.writeLine("==================")
+maqueen.motorStop(maqueen.Motors.All)
 basic.showIcon(IconNames.SmallSquare)
 basic.forever(function () {
     voiceRecognition.getCMDID()
@@ -13,6 +14,14 @@ basic.forever(function () {
             basic.showIcon(IconNames.Happy)
         } else if (voiceRecognition.readCMDID() == voiceRecognition.checkWord3(voiceRecognition.FixedCommandWords.W65)) {
             basic.clearScreen()
+        } else if (voiceRecognition.readCMDID() == voiceRecognition.checkWord2(voiceRecognition.LearningCommandWords.W5)) {
+            maqueen.motorStop(maqueen.Motors.All)
+            basic.showIcon(IconNames.No)
+        } else if (voiceRecognition.readCMDID() == voiceRecognition.checkWord3(voiceRecognition.FixedCommandWords.W22)) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
+            basic.showString("F")
+        } else {
+        	
         }
     }
 })
